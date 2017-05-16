b4ba7f0847eb41451112b5dee942d639ffa20349
Ricardo Chavarria <skumancer@gmail.com>
2017-05-16 01:48:20 -0500

    - added collision meshes - added new pickups and death items - using Unity car controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,55 +13,64 @@ public class PlayerController : MonoBehaviour
 	public Text countText;
 	public Text winText;
 	public Text timerText;
+	public Rigidbody rigidbody;
+	public ParticleSystem explosionEffect;
 
-	private Rigidbody rb;
 	private int count;
 
-	void Start ()
+	void Start()
 	{
-		rb = GetComponent <Rigidbody> ();
 		count = 0;
-		SetCountText ();
 		winText.text = "";
-		timerText.text = string.Format ("{0:####} seconds", timeRemaining);
+		timerText.text = string.Format("{0:####} seconds", timeRemaining);
+		SetCountText();
 	}
-
 
 	void Update()
 	{
 		timeRemaining -= Time.deltaTime;
-		if (timeRemaining <= 0.0f) {
-			GameManager.shared.PlayerDied ();
+		if (timeRemaining <= 0.0f)
+		{
+			GameManager.shared.PlayerDied();
 		}
 
-		timerText.text = string.Format ("{0:####} seconds", timeRemaining);
+		timerText.text = string.Format("{0:####} seconds", timeRemaining);
 	}
 
 	void OnTriggerEnter(Collider other) 
 	{
-		if (other.gameObject.CompareTag ("Pick Up")) 
+		if (other.gameObject.CompareTag("Pick Up")) 
 		{
-			other.gameObject.SetActive (false);
+			other.gameObject.SetActive(false);
 			count = count + 10;
 
 			timeRemaining += timePerItem;
 
-			SetCountText ();
+			SetCountText();
 		}
-		else if (other.gameObject.CompareTag ("Death"))
+		else if (other.gameObject.CompareTag("Death"))
 		{
-			Debug.Log ("Death");
-			transform.position = Vector3.zero;
-			GameManager.shared.PlayerDied ();
+			StartCoroutine("DeathSequence");
+
+			rigidbody.AddRelativeForce(500.0f * Vector3.up);
+			rigidbody.AddExplosionForce(2000.0f, other.gameObject.transform.position, 1500.0f, 1000.0f);
 		}
 	}
 
-	void SetCountText ()
+	private IEnumerator DeathSequence()
+	{
+		explosionEffect.Play();
+		yield return rigidbody.IsSleeping();
+
+		GameManager.shared.PlayerDied();
+	}
+	void SetCountText()
 	{		
 		countText.text = string.Format("Count: {0}", count);
-		if (count >= 100) {
+		if (count >= 100)
+		{
 			winText.text = "You Win!";
-			GameManager.shared.LoadNextLevel ();
+			GameManager.shared.LoadNextLevel();
 		}
 
 	}
---
+++ Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo
+{
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor;
+	public bool steering;
+}
+
+public class SimpleCarController : MonoBehaviour
+{
+	public List<AxleInfo> axleInfos; 
+	public float maxMotorTorque;
+	public float breakTorque;
+	public float maxSteeringAngle;
+	public GameObject centerOfMass;
+	public Rigidbody rigidbody;
+
+	public void Start()
+	{
+		rigidbody.centerOfMass = centerOfMass.transform.position;
+	}
+
+	public void FixedUpdate()
+	{
+		float motor = maxMotorTorque * Input.GetAxis("Vertical");
+		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+		float brake = Input.GetButton("Jump") ? breakTorque : 0.0f;
+
+		for (int i = 0; i < axleInfos.Count; ++i)
+		{
+			AxleInfo axleInfo = axleInfos[i];
+			if (axleInfo.steering)
+			{
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+
+			if (axleInfo.motor)
+			{
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+			}
+
+			axleInfo.leftWheel.brakeTorque = brake;
+			axleInfo.rightWheel.brakeTorque = brake;
+
+			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+		}
+	}
+
+	// finds the corresponding visual wheel
+	// correctly applies the transform
+	public void ApplyLocalPositionToVisuals(WheelCollider collider)
+	{
+		if (collider.transform.childCount == 0)
+		{
+			return;
+		}
+
+		Transform visualWheel = collider.transform.GetChild(0);
+
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose(out position, out rotation);
+
+		visualWheel.transform.position = position;
+		visualWheel.transform.rotation = rotation;
+	}
+}
