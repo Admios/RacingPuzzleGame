c6f21d0c7f783eed97be5fb9ff16e6b71409a0c7
Horacio Valdes <horacio.valdes@admios-sa.com>
2017-07-12 11:42:31 -0500

    Adding flight mode

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -270,35 +270,34 @@ namespace UnityStandardAssets.Vehicles.Car
         // these effects are controlled through the WheelEffects class
         private void CheckForWheelSpin()
         {
-			if (m_WheelEffects.Length == 4)
-			{
-				// loop through all wheels
-				for (int i = 0; i < 4; i++)
-				{
-					WheelHit wheelHit;
-					m_WheelColliders [i].GetGroundHit (out wheelHit);
+            // loop through all wheels
+            for (int i = 0; i < 4; i++)
+            {
+                WheelHit wheelHit;
+                m_WheelColliders[i].GetGroundHit(out wheelHit);
 
-					// is the tire slipping above the given threshhold
-					if (Mathf.Abs (wheelHit.forwardSlip) >= m_SlipLimit || Mathf.Abs (wheelHit.sidewaysSlip) >= m_SlipLimit) {
-						m_WheelEffects [i].EmitTyreSmoke ();
+                // is the tire slipping above the given threshhold
+                if (Mathf.Abs(wheelHit.forwardSlip) >= m_SlipLimit || Mathf.Abs(wheelHit.sidewaysSlip) >= m_SlipLimit)
+                {
+                    m_WheelEffects[i].EmitTyreSmoke();
 
-						// avoiding all four tires screeching at the same time
-						// if they do it can lead to some strange audio artefacts
-						if (!AnySkidSoundPlaying ())
-						{
-							m_WheelEffects [i].PlayAudio ();
-						}
-						continue;
-					}
+                    // avoiding all four tires screeching at the same time
+                    // if they do it can lead to some strange audio artefacts
+                    if (!AnySkidSoundPlaying())
+                    {
+                        m_WheelEffects[i].PlayAudio();
+                    }
+                    continue;
+                }
 
-					// if it wasnt slipping stop all the audio
-					if (m_WheelEffects [i].PlayingAudio) {
-						m_WheelEffects [i].StopAudio ();
-					}
-					// end the trail generation
-					m_WheelEffects [i].EndSkidTrail ();
-				}
-			}
+                // if it wasnt slipping stop all the audio
+                if (m_WheelEffects[i].PlayingAudio)
+                {
+                    m_WheelEffects[i].StopAudio();
+                }
+                // end the trail generation
+                m_WheelEffects[i].EndSkidTrail();
+            }
         }
 
         // crude traction control that reduces the power to wheel if the car is wheel spinning too much
